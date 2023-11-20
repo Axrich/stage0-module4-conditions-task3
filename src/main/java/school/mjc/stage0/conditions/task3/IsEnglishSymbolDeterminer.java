40f84dbd0d27304e241de0d907db7d6bc5cd3e76
Axrich <116589250+Axrich@users.noreply.github.com>
2023-11-20 20:31:22 +0500

    Update IsEnglishSymbolDeterminer.java

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+     
+        if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z')) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
+    }
+
+    public static void main(String[] args) {
+        IsEnglishSymbolDeterminer determiner = new IsEnglishSymbolDeterminer();
+
+        determiner.isEnglishSymbol('A');
+        determiner.isEnglishSymbol('8');
     }
 }
+
